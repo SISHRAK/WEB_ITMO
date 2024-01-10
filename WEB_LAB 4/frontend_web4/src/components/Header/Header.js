52b3a9b0972e6d9d4f6da8acfe69d55bdf7c36d6
Arseniy Barashko <94114454+SISHRAK@users.noreply.github.com>
2024-01-10 19:06:57 +0300

    Add files via upload

---
+++ WEB_LAB 4/frontend_web4/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <span>Web №4</span> <br/>
+            <span>Barashko Arseniy, P3234</span> <br/>
+        </div>
+    );
+};
+
+export default Header;
